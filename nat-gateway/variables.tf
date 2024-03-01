4ef62877e59448f3ddbad67033b88f22e0b43d71
kehindeomotayo <kennyjay_2@yahoo.com>
2024-03-01 13:14:22 +0000

    created nat-gateway module

---
+++ nat-gateway/variables.tf
@@ -0,0 +1,9 @@
+#variables for NAT
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_id" {}
+variable "internet_gateway" {}
+variable "public_subnet_az2_id" {}
+variable "vpc_id" {}
+variable "private_subnet_az1_id" {}
+variable "private_subnet_az2_id" {}
