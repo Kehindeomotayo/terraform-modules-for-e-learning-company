f3cfb24cfb45388a78edb828f65597bde2e01244
kehindeomotayo <kennyjay_2@yahoo.com>
2024-03-01 09:51:06 +0000

    vpc created for the e-learning platform

---
+++ vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "vpc_cidr" {}
+variable "project_name" {}
+variable "environment" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_subnet_az1_cidr" {}
+variable "private_subnet_az2_cidr" {}
