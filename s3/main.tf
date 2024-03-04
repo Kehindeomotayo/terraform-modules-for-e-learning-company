ab06002048e43000b40d0c989d06dedce4f104e5
kehindeomotayo <kennyjay_2@yahoo.com>
2024-03-04 16:27:36 +0000

    corrected s3

--- s3/main.tf
+++ s3/main.tf
@@ -7,9 +7,3 @@ resource "aws_s3_bucket" "env_file_bucket" {
   }
 }
 
-# upload the environment file from local computer into the s3 bucket
-resource "aws_s3_object" "upload_env_file" {
-  bucket = aws_s3_bucket.env_file_bucket.id
-  key    = var.env_file_name
-  source = "./${var.env_file_name}"
-}
--- s3/outputs.tf
+++ s3/outputs.tf
@@ -3,7 +3,3 @@ output "env_file_bucket_name" {
   value = var.env_file_bucket_name
 }
 
-# export the environment file name
-output "env_file_name" {
-  value = var.env_file_name
-}
